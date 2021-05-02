71eca268a7afda826d03f3ada73f558801206260
Caitlin Tighe <caitighe@gmail.com>
2021-05-02 01:51:04 -0400

    finished api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,38 @@
+
+//using express router the create route handlers
+const router = require('express').Router();
+const store = require('../db/store.js');
+
+
+// routes using GET POST DELETE
+router.get('/notes', function(req, res){
+    store
+        .getnotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+    store
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+router.delete('/notes/:id', function(req, res) {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
+
+
+
+
+
+
 const db = require('./db/db.json');
 const fs = require('fs');
 
@@ -12,30 +47,10 @@ function writeDB(note) {
     fs.writeFileSync('../db/db.json/', note, function(err) {
         if (err) {
             return console.log(err);
-            }
-        });
-        
+        }})
+    }
+};
 
-    });
-}
-router.get('public/notes.html', function(request, response) {
-    response.json(db);
-});
+app.get('/api/notes', function(req, res) {
 
-router.post('public/notes.html', function(request, response) {
-    let newNote = store(request.body);
-    db.push(newNote);
-    databaseWrite();
-    response.send(`Nice entry!`)
-});
-
-router.delete('public/notes.html:id', function(request, response) {
-    let noteID = request.params.id;
-    db = db.filter(entry => entry.id !== noteID);
-    databaseWrite();
-    response.send("Scrapped!")
-});
-
-
-
-module.exports = router;
+})
