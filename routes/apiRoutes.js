63ba360efc0d0960ccd8c25b0ba29b89edbd1e06
Caitlin Tighe <caitighe@gmail.com>
2021-05-01 23:51:58 -0400

    fixed api and HTML routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,41 @@
-let db = require('../db/db.json');
-const store = require('../js/store');
+const db = require('./db/db.json');
 const fs = require('fs');
-const router = require('express').Router();
-const path = require('path');
 
-router.get('/notes', function(request, response) {
+module.exports = function(app) {
+
+function writeDB(note) {
+    note = JSON.stringify(note);
+    console.log(note);
+
+//puts notes in the JSON file
+
+    fs.writeFileSync('../db/db.json/', note, function(err) {
+        if (err) {
+            return console.log(err);
+            }
+        });
+        
+
+    });
+}
+router.get('public/notes.html', function(request, response) {
     response.json(db);
 });
 
-router.post('/notes', function(request, response) {
+router.post('public/notes.html', function(request, response) {
     let newNote = store(request.body);
     db.push(newNote);
     databaseWrite();
     response.send(`Nice entry!`)
 });
 
-router.delete('/notes/:id', function(request, response) {
+router.delete('public/notes.html:id', function(request, response) {
     let noteID = request.params.id;
     db = db.filter(entry => entry.id !== noteID);
     databaseWrite();
     response.send("Scrapped!")
 });
 
-function databaseWrite() {
-    let note = JSON.stringify(db);
-    fs.writeFile(path.join(__dirname,'../db/db.json'), note, function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Yay");
-    });
-}
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 //created the modules being used
 
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
 const app = express();
 
-let PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
+// middleware to parse JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.use(express.static('public'));
+
+// path for routes
+app.use('./routes/apiRoutes')(app);
+app.use('./routes/htmlRoutes')(app);
+
+//listener
+app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+});
